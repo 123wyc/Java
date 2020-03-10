64f2e783f76942a0e55fde08624d6f6bef643641
wyc <wycdahaoren@163.com>
2020-03-10 16:37:12 +0800

    First commit

---
+++ src/main/java/com/example/wyc/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.wyc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping(value = "/test")
+public class TestController {
+
+
+    @RequestMapping(value = "/test1")
+    public String TestController(){
+
+        return "Success";
+    }
+}
---
+++ src/test/java/com/example/wyc/WycApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WycApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
