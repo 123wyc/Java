eea0eb37327b634b9e57a07ca3ecf5270714ac73
wangyichao <wangyichao@vchangyi.com>
2022-12-08 10:02:32 +0800

    测试提交

--- src/test/java/com/example/wyc/WycApplicationTests.java
+++ src/test/java/com/example/wyc/WycApplicationTests.java
@@ -3,11 +3,36 @@ package com.example.wyc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 @SpringBootTest
 class WycApplicationTests {
 
     @Test
     void contextLoads() {
+
+
+        Map<Integer, Integer> test = new HashMap<Integer, Integer>();
+        test.put(1, 2);
+        test.put(1, 1);
+
+        for (Integer key : test.keySet()
+        ) {
+
+            System.out.println(test.get(key));
+        }
+        Set<Integer> set = new HashSet<Integer>();
+
+        set.add(1);
+        set.add(1);
+
+        for (Integer value : set
+        ) {
+            System.out.println(value);
+        }
     }
 
 }
